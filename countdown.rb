3a831af826f5e8f40d6555d2298ce4b5d209a20e
Oscar Shoudt <shoudto@gmail.com>
2019-04-10 23:08:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(x)
   end
    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+  puts "#{x} SECONDS"
+  x -= 1
+  sleep(5.0)
+  end
+   return "HAPPY NEW YEAR!"
+end
